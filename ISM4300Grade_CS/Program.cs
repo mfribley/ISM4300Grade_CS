dd06d78a254919f2c96507cf4fb9329060a540ee
mfribley <mfribley@mail.usf.edu>
2019-01-21 22:33:47 -0500

    Add project files.

---
+++ ISM4300Grade_CS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISM4300Grade_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
